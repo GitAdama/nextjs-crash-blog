a90b99fb5f8e21392ecde88a5b0e819fbf40a682
GitAdama <Tyannick1996@gmail.com>
2022-06-23 09:53:54 +0200

    feat navigate-programatially: dynamic bloc page

--- components/blogItem/BlogItem.js
+++ components/blogItem/BlogItem.js
@@ -1,7 +1,16 @@
+import { useRouter } from 'next/router'
 
+
+//The blog item component for a blog
 function BlogItem(props) {
     const { title, image, description, details } = props
 
+    const router = useRouter()
+
+    //Dynamic navigation page for a blog 
+    const onNavigate = () =>{
+        router.push(`/${title}`)
+    }
     return (
         <div className="max-w-sm mx-auto overflow-hidden runded shadow-lg my-2" >
             <img className="w-full h-60" src={image} alt={title} />
@@ -10,7 +19,7 @@ function BlogItem(props) {
                 <p className="text-bas text-gray-700">{description}</p>
             </div>
             <div className="text-center">
-            <button className="px-4 py-2 my-1 font-semibold text-blue-700 bg-transparent border border-blue-500 rounded hover:bg-blue-500 hover:text-white hover:border-transparent">
+            <button onClick={onNavigate} className="px-4 py-2 my-1 font-semibold text-blue-700 bg-transparent border border-blue-500 rounded hover:bg-blue-500 hover:text-white hover:border-transparent">
                 Read More...
             </button> 
             </div>
