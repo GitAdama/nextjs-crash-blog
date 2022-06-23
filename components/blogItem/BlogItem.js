b2e2c9e83ada04bf655aa09b94851eb6538567ca
GitAdama <Tyannick1996@gmail.com>
2022-06-23 09:38:01 +0200

    feat rendering-dummy-data: render blog items

---
+++ components/blogItem/BlogItem.js
@@ -0,0 +1,21 @@
+
+function BlogItem(props) {
+    const { title, image, description, details } = props
+
+    return (
+        <div className="max-w-sm mx-auto overflow-hidden runded shadow-lg my-2" >
+            <img className="w-full h-60" src={image} alt={title} />
+            <div className="px-6 py-4">
+                <div className="mb-2 text-xl font-bold">{title}</div>
+                <p className="text-bas text-gray-700">{description}</p>
+            </div>
+            <div className="text-center">
+            <button className="px-4 py-2 my-1 font-semibold text-blue-700 bg-transparent border border-blue-500 rounded hover:bg-blue-500 hover:text-white hover:border-transparent">
+                Read More...
+            </button> 
+            </div>
+        </div >
+    )
+}
+
+export default BlogItem
--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,53 @@
 import Head from "next/head";
 import { Fragment } from "react";
+import BlogItem from "../components/blogItem/BlogItem";
 
+const BLOG_POSTS = [
+    {
+        id: 1,
+        title: "First Blog",
+        image: "https://images.unsplash.com/photo-1655833772095-13374a9c9bfc?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80",
+        description: "This is my first blog, really excited",
+        details: "How it all began..."
+    },
+    {
+        id: 2,
+        title: "Second Blog",
+        image: "https://images.unsplash.com/photo-1586919715453-3b9378bda31c?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwzOHx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=60",
+        description: "This is my second blog, a bit excited",
+        details: "I learn a bit from the first..."
+    },
+    {
+        id: 3,
+        title: "Third Blog",
+        image: "https://images.unsplash.com/photo-1655833018337-08bd2c4cc17b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHw0Nnx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=60",
+        description: "This is my third blog, the THIRDDDD",
+        details: "I am getting involved..."
+    }
+
+]
 function HomePage() {
-    return (<Fragment>
-        <Head>
-            <link
-                href="https://fonts.googleapis.com/css2?family=Montserrat&display=swap"
-                rel="stylesheet"
-            />
-        </Head>
-        <h1>Home Page</h1>
-    </Fragment>)
+
+    return (
+        <Fragment>
+            <Head>
+                <link
+                    href="https://fonts.googleapis.com/css2?family=Montserrat&display=swap"
+                    rel="stylesheet"
+                />
+            </Head>
+            <h1>Blog Page</h1>
+            {
+                BLOG_POSTS.map((blog) => (
+
+            <div key={blog.id} className="flex flex-col">
+                <BlogItem
+                    {...blog}
+                />
+            </div>
+            ))
+            }
+        </Fragment>
+    )
 }
 export default HomePage;
